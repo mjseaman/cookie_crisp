d5f4efef396382ab951e39d14d461e0b953944b9
Apprentice <apprentice@dbc21.local>
2013-08-06 17:38:28 -0700

    completed challenge

--- cookie_spec.rb
+++ cookie_spec.rb
@@ -22,13 +22,20 @@ describe Cookie do
   end
 
   describe "#type" do
-    it "returns the type of the cookie"
+    subject{ cookie.type }
+    # it "returns the type of the cookie" do
+    it { should eq("peanut butter") }
+
   end
 
   describe "#bake!" do
-    it "requires an integer time argument"
+    it "requires an integer time argument" do
+      expect { cookie.bake! }.to raise_error(ArgumentError)
+    end
 
-    it "returns the cookie object"
+    it "returns the cookie object" do
+      expect { cookie.bake!(10).should be_a(Cookie) }
+    end
 
     it "changes the status of the cookie when given enough time" do
       expect { cookie.bake!(10) }.to change(cookie, :status)
@@ -36,26 +43,44 @@ describe Cookie do
   end
 
   describe "#status" do
-    it "returns the cookie's current status"
+    subject { cookie.status }
+
+    it "returns the cookie's current status" do
+      expect{ subject is_not nil }
+    end
 
     context "when unbaked" do
-      it "is `:doughy`" 
+      # p cookie
+      # let(:cookie) { Cookie.new(type) }
+      it {should eq :doughy}
     end
 
     context "when baked for less than 7 minutes" do
-      it "is `:doughy`"
+      before do
+        cookie.bake!(6)
+      end
+      it {should eq :doughy}
     end  
 
     context "when baked for at least 7 but less than 10 minutes" do
-      it "is `:almost_ready`"
+      before do
+        cookie.bake!(8)
+      end
+      it {should eq :almost_ready}
     end
 
     context "when baked for at least 10 but less than 12 minutes" do
-      it "is `:ready`"
+      before do
+        cookie.bake!(11)
+      end
+      it {should eq :ready}
     end
 
     context "when baked for at least 12 minutes" do
-      it "is `:burned`"
+      before do
+        cookie.bake!(13)
+      end
+      it {should eq :burned}
     end
   end
 end
